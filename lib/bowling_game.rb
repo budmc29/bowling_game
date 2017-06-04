bd21e859fec7afa361ccf63a706e046b4cbbb3c9
Mugur (Bud) Chirica <chirica.mugurel@gmail.com>
2017-06-05 00:25:49 +0100

    Extract constant

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -2,6 +2,8 @@ require 'bowling_game/version'
 require 'pry'
 
 class BowlingGame
+  MAX_ROLL_POINTS = 10
+
   def initialize
     @rolls = []
   end
@@ -16,10 +18,10 @@ class BowlingGame
     first_in_frame = 0
     for frame in 0...10
 			if(strike?(first_in_frame))
-				score += 10 + @rolls[first_in_frame + 1] + @rolls[first_in_frame + 2]
+				score += MAX_ROLL_POINTS + @rolls[first_in_frame + 1] + @rolls[first_in_frame + 2]
         first_in_frame += 1
       elsif(spare?(first_in_frame))
-        score += 10 + @rolls[first_in_frame + 2]
+        score += MAX_ROLL_POINTS + @rolls[first_in_frame + 2]
         first_in_frame += 2
       else
 				score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
@@ -33,10 +35,10 @@ class BowlingGame
 	private
 
 	def spare?(first_in_frame)
-		(@rolls[first_in_frame] + @rolls[first_in_frame + 1]) == 10
+		(@rolls[first_in_frame] + @rolls[first_in_frame + 1]) == MAX_ROLL_POINTS
 	end
 
 	def strike?(first_in_frame)
-		@rolls[first_in_frame] == 10
+		@rolls[first_in_frame] == MAX_ROLL_POINTS
 	end
 end

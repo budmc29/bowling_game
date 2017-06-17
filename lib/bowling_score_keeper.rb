21d40fb9b213cf4d71a5772a3e3c83ad28ef9718
budmc29 <chirica.mugurel@gmail.com>
2017-06-17 22:48:33 +0100

    Extract score_for_too_few rolls method

--- lib/bowling_score_keeper.rb
+++ lib/bowling_score_keeper.rb
@@ -12,16 +12,16 @@ class BowlingScoreKeeper
   end
 
   def score
-    if @rolls.size <= 3
-      return @rolls.inject(0) { |s, e| s + e }
-    end
+    return score_for_too_few_rolls if too_few_rolls
 
     score = 0
-
     first_in_frame = 0
+
     10.times do
       if(strike?(first_in_frame))
-        score += MAX_ROLL_POINTS + @rolls[first_in_frame + 1] + @rolls[first_in_frame + 2]
+        score += MAX_ROLL_POINTS +
+          @rolls[first_in_frame + 1] +
+          @rolls[first_in_frame + 2]
         first_in_frame += 1
       elsif(spare?(first_in_frame))
         score += MAX_ROLL_POINTS + @rolls[first_in_frame + 2]
@@ -38,10 +38,19 @@ class BowlingScoreKeeper
   private
 
   def spare?(first_in_frame)
-    (@rolls[first_in_frame] + @rolls[first_in_frame + 1]) == MAX_ROLL_POINTS
+    (@rolls[first_in_frame] + @rolls[first_in_frame + 1]) ==
+      MAX_ROLL_POINTS
   end
 
   def strike?(first_in_frame)
     @rolls[first_in_frame] == MAX_ROLL_POINTS
+  end
+
+  def too_few_rolls
+    @rolls.size <= 3
+  end
+
+  def score_for_too_few_rolls
+    @rolls.inject(0) { |s, e| s + e }
   end
 end
